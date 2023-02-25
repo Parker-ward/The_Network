769c2e5c7b3560b61ea19d05c2e337aeddfad4c9
Parker <ward.parker26@gmail.com>
2023-02-24 17:03:06 -0700

    got post drawing to page

---
+++ src/pages/ProfilePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Hello in the profile page</div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
